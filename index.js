c79560907efdd8c8097de61253ddaadcb476d13c
Komenpurity <jepkorirpurity8@gmail.com>
2023-04-08 12:26:35 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Your code here
+//Create classes Cat, Dog, and Bird
+
+class Cat{
+    constructor(name,sex){
+        this.name = name;
+        this.sex = sex;
+    }
+    speak(){
+        return `${this.name} says meow! `
+    }
+}
+
+const cat = new Cat("Sasha", "female")
+
+class Dog{
+    constructor(name,sex){
+        this.name = name;
+        this.sex = sex;
+    }
+    speak(){
+        return `${this.name} says woof! `
+    }
+}
+const dog = new Dog("Rufio", "male")
+
+
+
+class Bird{
+    constructor(name,sex){
+        this.name = name;
+        this.sex = sex;
+    }
+    speak(){
+        if(this.sex === "male"){
+            return `It's me! ${this.name}, the parrot!`
+        } else {
+            return says `${this.name} says squawk!`
+        }
+    }
+}
+
+const bird = new Bird("Pablo", "male")
+const bird2 = new Bird("Mable", "female")
